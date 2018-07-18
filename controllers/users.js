a41fffba8713718e0ca3caf0275f4f2d1dbb4330
Philippe Luchansky <philuchansky@gmail.com>
2018-07-18 10:31:10 -0700

    update users controller to use new style of JSON response

--- controllers/users.js
+++ controllers/users.js
@@ -4,22 +4,24 @@ module.exports = {
 	// list all users
 	index: (req, res) => {
 		User.find({}, (err, users) => {
-			res.json(users)
+			if(err) return res.json({message: "ERROR", payload: null, code: err.code})
+			res.json({ message: "SUCCESS", payload: users})
 		})
 	},
 
 	// get one user
 	show: (req, res) => {
 		User.findById(req.params.id, (err, user) => {
-			res.json(user)
+			if(err) return res.json({message: "ERROR", payload: null, code: err.code})
+			res.json({ message: "SUCCESS", payload: user })
 		})
 	},
 
 	// create a new user
 	create: (req, res) => {
 		User.create(req.body, (err, user) => {
-			if(err) return res.json({success: false, code: err.code})
-			res.json({success: true, message: "User created.", user})
+			if(err) return res.json({message: "ERROR", payload: null, code: err.code})
+			res.json({ message: "SUCCESS", payload: user })
 		})
 	},
 
@@ -28,7 +30,8 @@ module.exports = {
 		User.findById(req.params.id, (err, user) => {
 			Object.assign(user, req.body)
 			user.save((err, updatedUser) => {
-				res.json({success: true, message: "User updated.", user})
+				if(err) return res.json({message: "ERROR", payload: null, code: err.code})
+				res.json({ message: "SUCCESS", payload: user })
 			})
 		})
 	},
@@ -36,7 +39,8 @@ module.exports = {
 	// delete an existing user
 	destroy: (req, res) => {
 		User.findByIdAndRemove(req.params.id, (err, user) => {
-			res.json({success: true, message: "User deleted.", user})
+			if(err) return res.json({message: "ERROR", payload: null, code: err.code})
+			res.json({ message: "SUCCESS", payload: user })
 		})
 	}
 }
